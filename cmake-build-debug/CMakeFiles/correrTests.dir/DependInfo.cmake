084d6fe91bf80a9c16279c9aea055f6e11368218
David Kuo <davee183@gmail.com>
2023-06-29 23:43:41 -0300

    Se agregan funciones a fachada.. Se corrigen los <T> que no iban

---
+++ cmake-build-debug/CMakeFiles/correrTests.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/src/Lollapatuza.cpp" "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/cmake-build-debug/CMakeFiles/correrTests.dir/src/Lollapatuza.cpp.obj"
+  "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/src/PuestosDeComida.cpp" "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/cmake-build-debug/CMakeFiles/correrTests.dir/src/PuestosDeComida.cpp.obj"
+  "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/src/fachada_lollapatuza.cpp" "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/cmake-build-debug/CMakeFiles/correrTests.dir/src/fachada_lollapatuza.cpp.obj"
+  "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/src/heap.cpp" "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/cmake-build-debug/CMakeFiles/correrTests.dir/src/heap.cpp.obj"
+  "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/tests/tests_lollapatuza.cpp" "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/cmake-build-debug/CMakeFiles/correrTests.dir/tests/tests_lollapatuza.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/cmake-build-debug/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "C:/Users/davee/OneDrive/Facu/Datos/AED 2/TP GRUPALES/algo2-tp3/cmake-build-debug/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/correrTests.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/correrTests.dir/src/Lollapatuza.cpp.obj"
+  "CMakeFiles/correrTests.dir/src/PuestosDeComida.cpp.obj"
+  "CMakeFiles/correrTests.dir/src/fachada_lollapatuza.cpp.obj"
+  "CMakeFiles/correrTests.dir/src/heap.cpp.obj"
+  "CMakeFiles/correrTests.dir/tests/tests_lollapatuza.cpp.obj"
+  "correrTests.exe"
+  "correrTests.exe.manifest"
+  "correrTests.pdb"
+  "libcorrerTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/correrTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
